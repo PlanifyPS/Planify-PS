bd1d5e121c58050b640f588231c9a32f607783ca
Joel Santana Aleman <26joelaleman@gmail.com>
2025-03-15 19:37:08 +0000

    Setting & Login

---
+++ Sources/JS/Setting.js
@@ -0,0 +1,20 @@
+function showTab(tab) {
+    document.getElementById('settings').style.display = tab === 'settings' ? 'block' : 'none';
+    document.getElementById('profile').style.display = tab === 'profile' ? 'block' : 'none';
+
+    let tabs = document.querySelectorAll('.tab');
+    tabs.forEach(el => el.classList.remove('active'));
+    document.querySelector(`[onclick="showTab('${tab}')"]`).classList.add('active');
+
+    let indicator = document.querySelector('.indicator');
+    indicator.style.transform = tab === 'settings' ? 'translateX(0%)' : 'translateX(100%)';
+}
+
+function toggleMode() {
+    const isDarkMode = document.getElementById('mode-toggle').checked;
+    if (isDarkMode) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
