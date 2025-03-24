658eaf0f559aa0329639fb2e5a6782fef9ff6a16
Dani <danielml2003.dml@gmail.com>
2025-03-24 23:32:11 +0000

    wip: analytics

--- frontend/src/js/analytics.js
+++ frontend/src/js/analytics.js
@@ -1,17 +1,50 @@
 import ProgressBar from 'https://cdn.jsdelivr.net/npm/progressbar.js/+esm';
 
+/// REVISAR: LAS GRAFICAS SOLO CARGAN AL REFRESCAR LA PAGINA Y NO CUANDO SE ACCEDE A ELLA
 document.addEventListener("DOMContentLoaded", function () {
-    let container = document.getElementById("progress-circle");
 
-    const bar = new ProgressBar.Circle(container, {
-        strokeWidth: 6,
+    localStorage.setItem("habitsJson", JSON.stringify(0.8))
+    localStorage.setItem("tasksJson", JSON.stringify(0.6))
+    let habits = document.getElementById("habits-circle-progress");
+    let tasks = document.getElementById("tasks-circle-progress");
+
+    const habitsCircleBar = new ProgressBar.Circle(habits, {
+        strokeWidth: 15,
         easing: "easeInOut",
-        duration: 1400,
+        duration: 1600,
         color: "#FFEA82",
-        trailColor: "#eee",
+        trailColor: "#0077B4",
         trailWidth: 1,
-        svgStyle: null
+        svgStyle: null,
+        text:{
+            value: '0%',
+            className: 'circle-progress-text',
+        }
+
+    });
+    const CircleBar = new ProgressBar.Circle(tasks, {
+        strokeWidth: 15,
+        easing: "easeInOut",
+        duration: 1600,
+        color: "#FFEA82",
+        trailColor: "#0077B4",
+        trailWidth: 1,
+        svgStyle: null,
+        text:{
+            value: '0%',
+            className: 'circle-progress-text',
+        }
+
     });
 
-    bar.animate(1.0);
+        habitsCircleBar.animate(localStorage.getItem("habitsJson"), {
+            step: function(state, bar) {
+                bar.setText(Math.round(bar.value() * 100) + '%');
+            }
+        });
+        CircleBar.animate(localStorage.getItem("tasksJson"), {
+            step: function(state, bar) {
+                bar.setText(Math.round(bar.value() * 100) + '%');
+            }
+        })
 });
