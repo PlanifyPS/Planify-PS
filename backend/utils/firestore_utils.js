3ed7f8be4f0d589a858852f18d84b56e048a31b4
PadriPudru <daniel.pa.wk@gmail.com>
2025-04-29 10:31:42 +0100

    Suma de puntos al completar challenges 2

--- backend/utils/firestore_utils.js
+++ backend/utils/firestore_utils.js
@@ -85,3 +85,24 @@ export async function getAllDocumentsFromCollection(collectionName) {
         console.error("Error getAllDocumentsFromCollection:", error);
     }
 }
+
+export const addPointsToUser = async (uid, pointsToAdd) => {
+    try {
+        const userRef = doc(db, "Users", uid);
+        const userSnap = await getDoc(userRef);
+
+        if (userSnap.exists()) {
+            const currentPoints = userSnap.data().points || 0;
+            const newPoints = currentPoints + pointsToAdd;
+
+            await updateDoc(userRef, { points: newPoints });
+
+            console.log(`Puntos actualizados en Firestore. Total: ${newPoints}`);
+            return newPoints;
+        } else {
+            console.error("Usuario no encontrado en Firestore");
+        }
+    } catch (error) {
+        console.error("Error al añadir puntos al usuario:", error);
+    }
+};
